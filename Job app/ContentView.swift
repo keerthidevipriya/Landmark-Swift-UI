308b2164d53a98df048d0d1a06574a0637098192
keerthigavara <keerthigavara@dbs.com>
2021-08-14 15:54:56 +0530

    Initial Commit

---
+++ Job app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Job app
+//
+//  Created by Keerthi Devi Priya GAVARA on 14/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Job app/Job_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Job_appApp.swift
+//  Job app
+//
+//  Created by Keerthi Devi Priya GAVARA on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Job_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
